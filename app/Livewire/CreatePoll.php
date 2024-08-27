9e256830757d142ccb938d038c23432eb3ad6712
Edward <78192184+F8TGT@users.noreply.github.com>
2024-08-27 08:55:47 -0600

    - display all the options available. - added styling to all the options available. - added a button to add the options.

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First'];
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption() { $this->options[] = ''; }
+
 }
